66587830222ac8f06b2acf31f8c434e786e6e53b
Luciane Conte <lucianeconte@gmail.com>
2019-04-07 17:04:01 -0300

    Finalizado

--- src/app/dashboard/imoveis/imovies-update/imovies-update.component.ts
+++ src/app/dashboard/imoveis/imovies-update/imovies-update.component.ts
@@ -11,16 +11,17 @@ import { Imovel } from 'src/app/modules/imovel/imovel';
   styleUrls: ['./imovies-update.component.scss']
 })
 export class ImoviesUpdateComponent implements OnInit {
-  imovel$: Observable<Imovel>
-  imovelForm: FormGroup
-  imovelSub: Subscription
+  imovel$: Observable<Imovel>;
+  imovelForm: FormGroup;
+  imovelSub: Subscription;
   constructor(
     private fb: FormBuilder,
     private service: ImovelService,
-    private route: ActivatedRoute,
-    private router: Router
+    private route: ActivatedRoute, //pega id
+    private router: Router //redireciona
   ) {
     this.imovelForm = this.fb.group({
+      _id: [null, Validators.required],
       tipo: [null, Validators.required],
       descricao: [null, Validators.required],
       locacao_venda: [null, Validators.required],
@@ -33,18 +34,19 @@ export class ImoviesUpdateComponent implements OnInit {
 
 
   ngOnInit() {
+    console.log('qui');
     this.route.params.subscribe((params: any) => {
       console.log(params)
       this.imovelSub = this.service.findOne(params.id)
        .subscribe((imovel: Imovel) => {
          console.table(imovel)
-         this.imovelForm.patchValue(imovel)
+         this.imovelForm.patchValue(imovel);
        })
     })
   }
 
   onSave() {
-    let {_id, ...data } = this.imovelForm.value
+    let {_id, ...data } = this.imovelForm.value;
     //console.log(_id)
     //console.table(data)
     this.service.update(_id, data).subscribe(
